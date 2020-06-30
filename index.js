a6df2095846e3053e89a772a7a5645450527e16b
Bernhard <37180819+bdorninger@users.noreply.github.com>
2020-06-30 16:16:27 +0200

    meaningless foo converter

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ $(() => {
 
         // You can specify inline conversions
         { from: 'cm', to: 'm', convertFunc: value => value / 100 },
+        { from: 'foo', to: 'bar', convertFunc: value => value + 1}
     ];
     
     function fromMeterToCentimeter(value) {
